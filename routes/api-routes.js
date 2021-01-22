7fda080e6456aebb9ec7cdaad3a503b7713d168d
Tracey Saada <tracey.saada@outlook.com>
2021-01-22 18:32:35 +0000

    added start of api.routes file

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,12 +1,17 @@
+const { route } = require("./html-routes");
+
 const router = require("express").Router();
 
+router.get("/notes", function (req, res) {
+  //this is where we need functionality which reads the db.json file & sends it back on the response
+});
 
+router.post("/notes", function (req, res) {
+  //this should receive a new note & save to db.json file
+});
 
+router.delete("/notes/:id", function (req, res) {
+  //this should take the id on the parameter & delete the note with this id
+});
 
-
-
-
-
-
-
-module.exports = router
+module.exports = router;
